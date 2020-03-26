0af04c093782785bb1cd54001c9bb228100150fc
Jorge Morales <jmorales113@gmail.com>
2020-03-26 13:55:21 -0700

    Created database/Tables Schema

---
+++ employeesDB.sql
@@ -0,0 +1,37 @@
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department(
+id INTEGER AUTO_INCREMENT NOT NULL,
+name VARCHAR(30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+id INTEGER AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(10,10) NOT NULL,
+department_id INTEGER NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee (
+id INTEGER AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INTEGER NOT NULL,
+manager_id INTEGER DEFAULT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id) REFERENCES role (id),
+FOREIGN KEY (manager_id) REFERENCES role (id)
+);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
+
+
+
+
