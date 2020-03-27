80d325d3f4779e4544c358c11b7107383dfc6b87
Jorge Morales <jmorales113@gmail.com>
2020-03-27 13:38:06 -0700

    Added Values and Joined the tables

--- seeds.sql
+++ seeds.sql
@@ -1,12 +1,36 @@
 INSERT INTO department (name)
 VALUES ("Sales"),
-("Sales"),
 ("Engineering"),
-("Engineering"),
-("Finance"),
+("Operations"),
 ("Legal"),
-("Legal"),
-("Engineering");
+("Human Resources"),
+("Marketing"),
+("Accounting"),
+("Customer Service");
+
+INSERT INTO role (department_id, title, salary)
+VALUES (1, "Sales Manager", 70000),
+(2, "Software Engineer", 60000),
+(3, "Operations Analyst", 40000),
+(4, "Lawyer", 200000),
+(5, "HR Manager", 50000),
+(6, "Marketing Associate", 55000),
+(7, "Accountant", 65000),
+(8, "Customer Service Rep", 35000);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Brian", "Meza", 1, NULL),
+("Jorge", "Morales", 2, 2),
+("Eddie", "Sanchez", 3, 3),
+("Fabiola", "Carrion", 4, 4),
+("Toby", "Flenderson", 5, NULL),
+("Kelly", "Kapoor", 6, 6),
+("Creed", "Bratton", 7, 7),
+("Meredith", "Palmer", 8, 8);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
 
 SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, employee.manager_id AS manager 
 FROM employees_db.employee
