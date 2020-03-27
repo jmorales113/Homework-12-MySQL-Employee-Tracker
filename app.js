d7299f5ae1ce3000c6996b01be1fe8ffa07f3c9d
Jorge Morales <jmorales113@gmail.com>
2020-03-27 14:27:19 -0700

    Added first function and switch/case statements

--- app.js
+++ app.js
@@ -17,11 +17,66 @@ let connection = mysql.createConnection({
 
   // Your password
   password: "annixter",
-  database: ""
+  database: "employees_DB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
+  startApp()
 //   connection.end();
 });
+
+function startApp() {
+  inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to do?",
+    options: [
+      "View All Employees",
+      "View All Employees by Department",
+      "Add Employee",
+      "Add Department",
+      "Update Employee Role",
+      "Delete Employee",
+      "Delete Department",
+      "Delete Employee Role"
+    ]
+  })
+  .then(function(answer){
+    switch(answer.action) {
+      case "View All Employees":
+        viewAllEmployees()
+      break
+
+      case "View All Employees by Department":
+      viewAllEmployeesByDepartment()
+      break
+
+      case "Add Employee":
+      addEmployee()
+      break
+
+      case "Add Department":
+      addDepartment()
+      break
+
+      case "Update Employee Role":
+      updateEmployeeRole()
+      break
+
+      case "Delete Employee":
+      deleteEmployee()
+      break
+
+      case "Delete Department":
+      deleteDepartment()
+      break
+
+      case "Delete Employee Role":
+      deleteEmployeeRole()
+      break
+    }
+  })
+}
